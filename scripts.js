fea23bed18f7d275fbc97610437b7b681a4ef260
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-11-04 17:15:35 -0500

    implement forEach method

--- scripts.js
+++ scripts.js
@@ -25,9 +25,36 @@ async function getRandomUser() {
     };
 
     addData(newUser);
+    // console.log(user)
 }
 
 //Add new object to data array
 function addData(obj){
     data.push(obj);
+
+    updateDOM();
 }
+
+//Update DOM
+function updateDOM(providedData = data){
+    //Clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element); 
+    });
+}
+
+//Format number as money
+function formatMoney(number){
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//Event Listeners 
+addUserBtn.addEventListener('click', getRandomUser);
+
+
+  
